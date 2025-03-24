805630cdf4a5438b3eb629503ac235e7517abb39
BhushanMahadik <149048349+BhushanMahadik@users.noreply.github.com>
2025-03-24 18:36:18 +0530

    Category Dto files added successully.

---
+++ dto/CategoryRequestDto.java
@@ -0,0 +1,12 @@
+package com.TrendHive.TrendHive.dto.CategoryDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryRequestDto {
+    private String name;
+}
---
+++ dto/CategoryResponseDto.java
@@ -0,0 +1,13 @@
+package com.TrendHive.TrendHive.dto.CategoryDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryResponseDto {
+    private int id;
+    private String name;
+}
