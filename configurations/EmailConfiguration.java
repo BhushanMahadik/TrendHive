bc76299f00e125934b29ef57afcdbe27e7b6990e
BhushanMahadik <149048349+BhushanMahadik@users.noreply.github.com>
2025-03-24 18:28:21 +0530

    configuration files added successfully

---
+++ configurations/EmailConfiguration.java
@@ -0,0 +1,9 @@
+package com.TrendHive.TrendHive.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class EmailConfiguration {
+}
